6993b913ddef49f38470795ce33b388831546ae3
arnoprincestonpan <pan.arno@gmail.com>
2025-02-07 16:54:52 -0800

    make format look better - added Separator Component, color to complete, edit, del buttons, used flexbox styles to properly format app

--- App.js
+++ App.js
@@ -1,31 +1,49 @@
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { Separator, StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
 
-  const goals = [
+  const Separator = () => <View style={styles.separator}/>;
+
+  const [goals, setGoals] = useState([
     { id: 1, date: 1696886400000, text: "Take a shower.", completed: false},
     { id: 2, date: 1696886400000, text: "Read a book.", completed: false},
     { id: 3, date: 1696713600000, text: "Eat some food.", completed: false},
     { id: 4, date: 1696627200000, text: "Clean the apartment.", completed: false},
     { id: 5, date: 1696886400000, text: "Talk to friends and family.", completed: false}
-  ];
+  ]);
+
+  const [newGoal, setNewGoal] = useState("");
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Daily Focus Tracker</Text>
       <View>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your focus goal."
-        />
-        <Button title="Add Goal"/>
+        <View style={styles.addCard}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your focus goal."
+          />
+          <Button title="Add Goal"/>
+        </View>
+        <Separator/>
         <Text style={styles.subtitle}>Today's Goals</Text>
         <FlatList
           data={goals}
           keyExtractor={item => item.id.toString()}
           renderItem={({item}) => (
-            <View>
+            <View style={styles.goalItems}>
                 <Text>{item.text}</Text>
+
+                <View style={styles.goalButtons}>
+                  {item.completed ? 
+                    <Button color="green" title="Completed"/>
+                    :
+                    <Button color="red" title="InComplete"/>
+                  }
+                  <Button color="#FF7700" title="Edit"/>
+                  <Button color="#c00000" title="Delete"/>
+                </View>
             </View>
           )}
         />
@@ -45,6 +63,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
+  addCard: {
+    flexDirection: "row",
+    justifyContent: 'space-around',
+  },
   input: {
     flex: 1,
     borderWidth: 1,
@@ -53,5 +75,24 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 18,
     marginBottom: 10,
-  }
+  },
+  goalItems: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: 'center',
+    padding: 10,
+  },
+  goalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  goalButton: {
+    padding: 10,
+  },
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 });
